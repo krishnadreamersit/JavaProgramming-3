de5aa724dd3497a3e2de8e1b1882ba09793e047f
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-02-28 08:50:53 +0545

    Feb 28, 2022

--- PoPLectureB/src/week5_lab_1/CalculatorTest.java
+++ PoPLectureB/src/week5_lab_1/CalculatorTest.java
@@ -6,10 +6,27 @@ import mylibrary.MyCalculator;
 public class CalculatorTest {
 
 	public static void main(String[] args) {
+		
 		Calculator c1 = new Calculator(6,7);
 		Calculator c2;
 		MyCalculator mc = new MyCalculator();
+		
 		c2 = mc.calcSum(c1);
+		System.out.println(c2);
+		
+		c2 = mc.calcSub(c1);
+		System.out.println(c2);
+		
+		c2 = mc.calcPrd(c1);
+		System.out.println(c2);
+		
+		c2 = mc.calcDiv(c1);
+		System.out.println(c2);
+		
+		c2 = mc.calcRem(c1);
+		System.out.println(c2);
+		
+		c2 = mc.calcPow(c1);
 		System.out.println(c2);
 	}
 }

9e177e0418cc187d540ec3b0d214ef2452359a82
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-02-28 11:38:07 +0545

    Feb 28, 2022

--- PopLectureA/src/mylibrary/MyCalculator.java
+++ PopLectureA/src/mylibrary/MyCalculator.java
@@ -6,4 +6,31 @@ public class MyCalculator {
 		c.setNum3(c.getNum1()+c.getNum2());
 		return c;
 	}
+	//sub
+	public Calculator sub(Calculator c) {
+		c.setNum3(c.getNum1()-c.getNum2());
+		return c;
+	}
+	
+	//prd
+	public Calculator prd(Calculator c) {
+		c.setNum3(c.getNum1()*c.getNum2());
+		return c;
+	}
+	
+	//div
+	public Calculator div(Calculator c) {
+		c.setNum3(c.getNum1()/c.getNum2());
+		return c;
+	}
+	//rem
+	public Calculator rem(Calculator c) {
+		c.setNum3(c.getNum1()%c.getNum2());
+		return c;
+	}
+	//pow
+	public Calculator pow(Calculator c) {
+		c.setNum3((int) Math.pow(c.getNum1(), c.getNum2()));
+		return c;
+	}
 }
--- PopLectureA/src/week5_lab_1/CalculatorTest.java
+++ PopLectureA/src/week5_lab_1/CalculatorTest.java
@@ -12,7 +12,23 @@ public class CalculatorTest {
 		Calculator c1 = new Calculator(4,9);
 		Calculator c2;
 		MyCalculator mc = new MyCalculator();
+		
 		c2 = mc.add(c1);
 		System.out.println(c2);		
+		
+		c2 = mc.sub(c1);
+		System.out.println(c2);
+		
+		c2 = mc.prd(c1);
+		System.out.println(c2);
+		
+		c2 = mc.div(c1);
+		System.out.println(c2);
+		
+		c2 = mc.rem(c1);
+		System.out.println(c2);
+		
+		c2 = mc.pow(c1);
+		System.out.println(c2);
 	}
 }
